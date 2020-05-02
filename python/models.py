ce47f6eeec1ebe4d1a73c50f80060f8553897671
ricsi98 <98kissricsi@gmail.com>
2020-05-02 11:27:53 +0200

    refactor

---
+++ python/models.py
@@ -0,0 +1,30 @@
+import random
+import itertools
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+ACTIONS = [x for x in itertools.product([-1,0,1], [-1,0,1])]
+
+class DQN(nn.Module):
+    def __init__(self, layers):
+        super(DQN, self).__init__()
+        self.layers = []
+        for i, l in enumerate(layers[:-1]):
+            self.layers.append(nn.Linear(l,layers[i+1]).double())
+            self.add_module("layer"+str(i), self.layers[-1])
+        
+    
+    def forward(self, x):
+        for l in self.layers:
+            x = F.relu(l(x))
+        return x
+
+    def learn(self, episode):
+        pass
+
+    def getAction(self, obs, epsilon):
+        if np.random.uniform() < epsilon:
+            return random.choice(ACTIONS)
+        return ACTIONS[torch.multinomial(self.forward(obs), 1).item()]
--- python/test.py
+++ python/test.py
@@ -1,34 +1,7 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from env import CarEnv
-import numpy as np
-import random
-import itertools
+import torch
+from models import *
 
-ACTIONS = [x for x in itertools.product([-1,0,1], [-1,0,1])]
-
-class DQN(nn.Module):
-    def __init__(self, layers):
-        super(DQN, self).__init__()
-        self.layers = []
-        for i, l in enumerate(layers[:-1]):
-            self.layers.append(nn.Linear(l,layers[i+1]).double())
-            self.add_module("layer"+str(i), self.layers[-1])
-        
-    
-    def forward(self, x):
-        for l in self.layers:
-            x = F.relu(l(x))
-        return x
-
-    def learn(self, episode):
-        pass
-
-    def getAction(self, obs, epsilon):
-        if np.random.uniform() < epsilon:
-            return random.choice(ACTIONS)
-        return ACTIONS[torch.multinomial(self.forward(obs), 1).item()]
 
 if __name__ == '__main__':
     try:
